a2a64300dee21fe4e39e40f154f4e76d16c218a6
MohJamal <mohjamalalkhatib96@gmail.com>
2025-08-10 10:56:00 +0300

    02 - Views, Blade Templates and Layouts => start into layouts and dashborads parts inside views directory + insert html template inside the laravel app

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    // Actions
+    public function index()
+    {
+        $title = 'Store';
+
+        // Return response: view, json, redirect, file
+        return view('dashboard.index', [
+            'user' => 'Mohammedjamal',
+            'title' => $title
+        ]);
+    }
+}
